c492e60e3c84e91c120654aba273350ca771ca9a
Jonas Bandi <dev@jonasbandi.net>
2018-09-13 12:13:51 +0200

    Adding THREE and three-bmfont-text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import THREE from 'three';
+import './globalThree';
+import createTextGeometry from 'three-bmfont-text';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'three-project';
+
+  ngOnInit(): void {
+
+    console.log(THREE);
+    console.log(createTextGeometry);
+  }
 }
+
+
+
